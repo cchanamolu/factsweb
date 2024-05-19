e094dea445f3f3739fc478fcb43368eeaf5980de
Charitha <cchanamolu@outlook.com>
2024-05-19 12:24:20 -0700

    Facts Web

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# factsweb
+# FactFuse: A Collaborative Fact Sharing Platform
+
+## Description
+
+FactFuse is a dynamic platform designed to bring together users passionate about sharing and discovering interesting facts in specific categories. With a sleek and intuitive user interface crafted using HTML, CSS, and JavaScript, users can effortlessly contribute their knowledge and engage with a vibrant community.
+
+## Key Features
+
+- **Fact Submission**: Users can submit facts related to various categories, enriching the platform's knowledge base.
+- **Voting System**: Other users can browse through submitted facts and vote for their favorites, promoting the most intriguing and accurate information.
+- **Category Organization**: Facts are organized by specific categories, allowing users to explore topics of interest easily.
+- **Responsive Design**: The user interface is optimized for seamless browsing across devices, ensuring a consistent experience for all users.
+
+## Backend Infrastructure
+
+Powered by Supabase, the backend of FactFuse ensures robust data management and user authentication. Supabase provides a scalable and secure foundation, allowing for seamless integration with the frontend and efficient handling of user interactions.
+
+## Why FactFuse?
+
+FactFuse aims to foster a community-driven environment where knowledge-sharing is celebrated and rewarded. By leveraging modern web technologies and a powerful backend infrastructure, FactFuse empowers users to curate and discover fascinating facts, enriching their understanding of diverse topics.
+
+## Get Involved
+
+contribute, and vote on intriguing facts across various categories. Together, let's build a repository of knowledge that inspires and enlightens!
---
+++ script.js
@@ -0,0 +1,75 @@
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
+// Selecting DOM elements
+const btn = document.querySelector(".fact-button");
+const form = document.querySelector(".factForms");
+const factsList = document.querySelector(".facts-list");
+// we can display the dom element in the console using console.dir(DOM Element)
+
+// Create DOM elements: Render facts in List
+factsList.innerHTML = " ";
+// Load data from the intialFactsArray provided above
+//createFactsList(initialFacts);
+
+// Load Data from supabase
+loadFacts();
+
+async function loadFacts() {
+  const res = await fetch(
+    "https://uoyqfgpspuinmteldjbs.supabase.co/rest/v1/facts",
+    {
+      headers: {
+        apikey:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVveXFmZ3BzcHVpbm10ZWxkamJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU1NjEzNDksImV4cCI6MjAzMTEzNzM0OX0.MLZsvHLL42NiKvwsl24uxkLhwuKzXadkoCxKOVtTDlk",
+        authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVveXFmZ3BzcHVpbm10ZWxkamJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU1NjEzNDksImV4cCI6MjAzMTEzNzM0OX0.MLZsvHLL42NiKvwsl24uxkLhwuKzXadkoCxKOVtTDlk",
+      },
+    }
+  );
+  const data = await res.json();
+
+  createFactsList(data);
+}
+
+function createFactsList(dataArray) {
+  const htmlArray = dataArray.map(
+    (fact) => `<li class="fact">
+          <p> ${fact.text}
+              <a
+                  class="source"
+                  href= ${fact.source}
+                  target="_blank"
+              >(Source)</a>
+          </p>
+          <span class="tag" style="background-color: ${
+            CATEGORIES.find((cat) => cat.name === fact.category).color
+          }"
+              >${fact.category}</span
+                    ></li>`
+  );
+  factsList.insertAdjacentHTML("afterbegin", htmlArray);
+}
+
+// Add button function to display form on click operation
+// Toggle Form visibility
+btn.addEventListener("click", function () {
+  if (form.classList.contains("hidden")) {
+    form.classList.remove("hidden");
+    btn.textContent = "Close";
+  } else {
+    form.classList.add("hidden");
+    btn.textContent = "Share a fact";
+  }
+});
+
+const times = [-1, -2, 1, 2, 3, 4].find((el) => el > 0);
+console.log(times);
